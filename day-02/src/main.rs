8b0af9d148182971dddd355d97aa17ffa77cef06
Michael Burleson <mike.burleson@me.com>
2022-12-02 13:44:43 -0500

    Day 02 - Part 2 - First attempt

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -10,15 +10,15 @@ fn main() {
 	let mut scores = HashMap::new();
 	let mut total_score = 0;
 	
-	scores.insert(String::from("A X"), 4); // RvR 1 + 3
-	scores.insert(String::from("A Y"), 8); // RvP 2 + 6
-	scores.insert(String::from("A Z"), 3); // RvS 3 + 0
-	scores.insert(String::from("B X"), 1); // PvR 1 + 0
-	scores.insert(String::from("B Y"), 5); // PvP 2 + 3
-	scores.insert(String::from("B Z"), 9); // PvS 3 + 6
-	scores.insert(String::from("C X"), 7); // SvR 1 + 6
-	scores.insert(String::from("C Y"), 2); // SvP 2 + 0
-	scores.insert(String::from("C Z"), 6); // SvS 3 + 3
+	scores.insert(String::from("A X"), 3); // Lose : RvS 3 + 0
+	scores.insert(String::from("A Y"), 4); // Draw : RvR 1 + 3
+	scores.insert(String::from("A Z"), 8); // Win : RvP 2 + 6
+	scores.insert(String::from("B X"), 1); // Lose : PvR 1 + 0
+	scores.insert(String::from("B Y"), 5); // Draw : PvP 2 + 3
+	scores.insert(String::from("B Z"), 9); // Win : PvS 3 + 6
+	scores.insert(String::from("C X"), 2); // Lose : SvP 2 + 0
+	scores.insert(String::from("C Y"), 6); // Draw : SvS 3 + 3
+	scores.insert(String::from("C Z"), 7); // Win : SvR 1 + 6
 	
 	for line in f.lines() {
 		let line = line.expect("We should be able to read strings from the file");

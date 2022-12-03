6c558305fce7ce470133c7dec80423785247fe52
Michael Burleson <mike.burleson@me.com>
2022-12-03 15:51:47 -0500

    Day 03 - Part 2 - First attempt

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -2,6 +2,17 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 fn main() {
+	fn authorize(line1: String, line2: String, line3: String) -> u32 {
+		let values = line1.chars();
+		let mut p_score = 0;
+		for c in values {
+			if !line2.find(c).is_none() && !line3.find(c).is_none() {
+				p_score = score(c);
+			}
+		}
+		p_score
+	}
+	
 	fn compare(line1: String, line2: String) -> u32 {
 		let values = line1.chars();
 		let mut p_score = 0;
@@ -27,17 +38,21 @@ fn main() {
 	let f = BufReader::new(f);
 	
 	let mut total_priority_score = 0;
+	let mut line_count = 0;
+	let mut line1 = "".to_string();
+	let mut line2 = "".to_string();
+	let mut line3 = "".to_string();
 	
 	for line in f.lines() {
 		let line = line.expect("We should be able to read strings from the file");
-		if line.trim().is_empty() {
-			// Do nothing
+		line_count = line_count + 1;
+		if line_count % 3 == 1 {
+			line1 = line;
+		} else if line_count % 3 == 2 {
+			line2 = line;
 		} else {
-			let pack_size = line.chars().count();
-			let compartment_size = pack_size / 2;
-			let line1 = &line[0..compartment_size];
-			let line2 = &line[compartment_size..pack_size];
-			total_priority_score = total_priority_score + compare(line1.to_string(), line2.to_string());
+			line3 = line;
+			total_priority_score = total_priority_score + authorize(line1.to_string(), line2.to_string(), line3.to_string());
 		}
 	}
 	
